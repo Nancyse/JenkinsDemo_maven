57fdf7b8aabbeb8f223de1b0c2a37a5579f0b423
01377990 <01377990@SF0001377990A.sf.com>
2018-07-24 19:40:56 +0800

    修改了APP类，增加了helloworld类。

---
+++ src/main/java/JenkinsTest/jenkinsDemo/HelloWorld.java
@@ -0,0 +1,11 @@
+package JenkinsTest.jenkinsDemo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello hello");
+
+	}
+
+}
