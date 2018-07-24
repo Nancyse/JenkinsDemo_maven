854ccd72b88d99f51aa97f36649c9d4e383c8f87
01377990 <01377990@SF0001377990A.sf.com>
2018-07-24 20:00:19 +0800

    修改了APP类和helloworld类

--- src/main/java/JenkinsTest/jenkinsDemo/App.java
+++ src/main/java/JenkinsTest/jenkinsDemo/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello app !");
+		System.out.println("hello hello app !");
 
 	}
 }
