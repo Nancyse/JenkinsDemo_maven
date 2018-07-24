1dcd6faf4d9813ed3b680a8061b7e1288e5ffc3b
01377990 <01377990@SF0001377990A.sf.com>
2018-07-24 16:58:23 +0800

    打印hello world!

---
+++ src/JenkinsTest/jenkinsDemo/App.java
@@ -0,0 +1,14 @@
+package JenkinsTest.jenkinsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+
+	}
+}
